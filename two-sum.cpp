a74d50142284a23af6fea5e15eedc5c49e9fde42
sabdulmajid <ayman.hasib@outlook.com>
2023-05-15 18:47:14 +0300

    Two sum code

---
+++ two-sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer;
+        int size = nums.capacity();
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i+1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+};
