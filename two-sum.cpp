6adcf60421c5bdc672d30b16007b5f621e4f2ca2
sabdulmajid <ayman.hasib@outlook.com>
2023-05-15 18:51:29 +0300

    Added explanation to two sum

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
+/*
+The function works by iterating over the array, pair-wise, and checking if the sum of the two numbers is equal to the target. If it is, the function returns the indices of the two numbers. If it is not, the function continues iterating. The function returns an empty vector if no two numbers in the array add up to the target.
+
+Here is a more detailed explanation of the steps the function takes:
+
+    1. The function creates a vector to store the indices of the two numbers.
+    2. The function iterates over the array, pair-wise.
+    3. For each pair of numbers, the function checks if the sum of the two numbers is equal to the target.
+    4. If the sum of the two numbers is equal to the target, the function adds the indices of the two numbers to the vector.
+    5. The function returns the vector.
+
+*/
 
 class Solution {
 public:
