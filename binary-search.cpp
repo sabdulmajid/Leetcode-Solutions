8ecd1e91ffed1cd31edcdbd46e4d3b7c3793660e
sabdulmajid <ayman.hasib@outlook.com>
2023-05-17 22:50:57 +0300

    solution using for loop

--- binary-search.cpp
+++ binary-search.cpp
@@ -6,6 +6,9 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        for (int it = 0; it < nums.size(); it++) {
+            if (nums[it] == target) return it; 
+        }
+        return -1;
     }
 };
