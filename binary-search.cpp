57c65ee2bff81d1f2419ee415eecf2b5641a1215
sabdulmajid <ayman.hasib@outlook.com>
2023-05-17 22:55:37 +0300

    solution explanation for future reference

--- binary-search.cpp
+++ binary-search.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+/*
+
+The search() method iterates through the vector and compares each element to the target integer. If the target integer is found, the method returns the index of the element. If the target integer is not found, the method returns -1.
+
+The search() method can be used to find the index of a target integer in a vector. The method works by iterating through the vector and comparing each element to the target integer. If the target integer is found, the method returns the index of the element. If the target integer is not found, the method returns -1.
+
+*/
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
