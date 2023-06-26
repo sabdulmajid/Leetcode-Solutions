79affe3814dede95e8a02c581c3f518dcdb94516
sabdulmajid <ayman.hasib@outlook.com>
2023-06-26 14:23:16 +0600

    code explanation

--- lowest_common_ancestor.cpp
+++ lowest_common_ancestor.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+/*
+
+The function first checks if the root node is null. If it is, then the LCA is null, because there is no ancestor of two nodes that do not exist. 
+Otherwise, the function checks if the root node's value is greater than both of the nodes' values. If it is, then the LCA must be in the left subtree of the root node, so the function recursively calls itself on the left subtree. 
+Similarly, if the root node's value is less than both of the nodes' values, then the LCA must be in the right subtree of the root node, so the function recursively calls itself on the right subtree. Otherwise, the root node must be the LCA, so the function returns the root node.
+
+*/
 
 // Definition for a binary tree node.
 struct TreeNode {
