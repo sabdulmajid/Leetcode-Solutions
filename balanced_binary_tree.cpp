e77451c38104970d72da5d3d746120405e6dacbc
sabdulmajid <ayman.hasib@outlook.com>
2023-06-27 02:16:42 +0600

    code explanation for the solution (for future reference)

--- balanced_binary_tree.cpp
+++ balanced_binary_tree.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+/*
+
+The function is called isBalanced() and it takes a pointer to the root node of the binary tree as input. The function first checks if the root node is null. If it is, then the tree is trivially balanced, so the function returns true. Otherwise, the function calls the Height() function to recursively calculate the height of the left and right subtrees of the root node.
+
+The Height() function takes a pointer to a node as input and returns the height of the subtree rooted at that node. The height of a node is defined as the number of edges from the node to the furthest leaf node.
+
+If the height of the left and right subtrees of the root node are both equal to 0, then the root node is a leaf node and the tree is balanced. Otherwise, if the height of the left and right subtrees are equal, then the tree is balanced. Otherwise, if the height of the left and right subtrees differ by more than 1, then the tree is not balanced and the function returns false.
+
+Finally, if the function reaches this point, then the tree is balanced and the function returns true.
+
+*/
+
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
