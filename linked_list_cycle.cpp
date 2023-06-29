9d6883b8e7629cf7d2642650f07fecb902ddfe8d
sabdulmajid <ayman.hasib@outlook.com>
2023-06-29 20:13:40 +0600

    linked list cycle skeleton code

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+    }
+};
