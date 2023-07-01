8abd2516182cf751e80e8e7e22f429f5f3ba33e6
sabdulmajid <ayman.hasib@outlook.com>
2023-07-01 23:16:06 +0600

    code explanation added to code for future reference

--- linked_list_cycle.cpp
+++ linked_list_cycle.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+/*
+
+The function has two pointers, slow and fast, that start at the head of the linked list. The slow pointer moves one node at a time, while the fast pointer moves two nodes at a time. If the two pointers ever meet, then there is a cycle in the linked list. If the two pointers never meet, then there is no cycle in the linked list.
+
+The function works by repeatedly moving the slow and fast pointers until one of the following two conditions is met:
+
+● The fast pointer is NULL, which means that the end of the linked list has been reached. In this case, there is no cycle in the linked list.
+● The slow pointer and the fast pointer point to the same node, which means that there is a cycle in the linked list.
+
+In either case, the function returns the appropriate boolean value.
+
+*/
+
 // Definition for singly-linked list
 struct ListNode {
     int val;
