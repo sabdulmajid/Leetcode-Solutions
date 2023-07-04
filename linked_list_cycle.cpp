26191e195c97fdaf0edde8ad13977da8dc6f0478
sabdulmajid <ayman.hasib@outlook.com>
2023-07-04 23:52:15 +0600

    changed bullet point formatting for better UI/UX experience

--- linked_list_cycle.cpp
+++ linked_list_cycle.cpp
@@ -7,8 +7,8 @@ The function has two pointers, slow and fast, that start at the head of the link
 
 The function works by repeatedly moving the slow and fast pointers until one of the following two conditions is met:
 
-● The fast pointer is NULL, which means that the end of the linked list has been reached. In this case, there is no cycle in the linked list.
-● The slow pointer and the fast pointer point to the same node, which means that there is a cycle in the linked list.
+• The fast pointer is NULL, which means that the end of the linked list has been reached. In this case, there is no cycle in the linked list.
+• The slow pointer and the fast pointer point to the same node, which means that there is a cycle in the linked list.
 
 In either case, the function returns the appropriate boolean value.
 
