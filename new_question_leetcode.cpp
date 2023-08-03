c71fe4c33f4b9c8c7b3703b333e204926064e655
sabdulmajid <ayman.hasib@outlook.com>
2023-08-03 00:14:57 -0400

    working on implementing Queue with Stack functions

--- new_question_leetcode.cpp
+++ new_question_leetcode.cpp
@@ -1,3 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+    stack<int> inp, out;
+public:
+    MyQueue() {}
+    
+    void push(int x) {
+        inp.push(x);
+    }
+    
+    int pop() {
+        if (out.empty()) {
+            while (!inp.empty()) {
+                out.push(inp.top());
+                inp.pop();
+            }
+        }
+        int temp = out.top();
+        out.pop();
+        return temp;
+    }
+    
+    int peek() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
