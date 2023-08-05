99ca5eeeb2040803f1217e1b429851248a1ae5c1
sabdulmajid <ayman.hasib@outlook.com>
2023-08-05 18:50:26 -0400

    solution to first bad version problem

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -4,6 +4,18 @@ bool isBadVersion(int version);
 class Solution {
 public:
     int firstBadVersion(int n) {
+        if(n == 1) return n;
+
+        int low = 1;
+        int high = n;
         
+        while(low < high)
+        {
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+
     }
 };
