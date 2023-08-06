e92f491d782cc58c607e0a236588366f9d8e19fd
sabdulmajid <ayman.hasib@outlook.com>
2023-08-06 19:20:14 -0400

    added libraries and code explanation

--- first_bad_version.cpp
+++ first_bad_version.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+/* Code Explanation
+
+This code defines a class Solution that contains a method firstBadVersion(n) which uses binary search to efficiently find the first occurrence of a "bad version" among a range of versions from 1 to n. The method iteratively narrows down the search range by dividing it in half until it finds the first bad version. It uses the isBadVersion() API to check if a given version is bad or not. The algorithm maintains two pointers, low and high, and updates them based on the result of isBadVersion() to efficiently find the first bad version in logarithmic time.
+
+*/
+
 // The API isBadVersion is defined for you.
 bool isBadVersion(int version);
 
