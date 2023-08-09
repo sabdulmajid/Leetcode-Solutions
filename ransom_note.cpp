97218719688f8cbb70a628d105382744c41dc418
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-08-09 02:17:21 -0400

    added sample solution

--- ransom_note.cpp
+++ ransom_note.cpp
@@ -4,4 +4,21 @@
 using namespace std;
 
 /* Code Explanation
+
 */
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> count(26, 0);
+        for (char c : magazine) {
+            count[c - 'a']++;
+        }
+        for (char c : ransomNote) {
+            if (--count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
