d6228b4749e6c0fba585a0c8dc3dd6959b40ba9b
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-08-11 00:03:06 -0400

    added code explanation

--- climbing_stairs.cpp
+++ climbing_stairs.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+/* Code Explanation
+
+The memoization solution avoids redundant calculations. We use an unordered map (memo) to store the already computed results for each step n. Before making a recursive call, we check if the result for the given n exists in the memo. If it does, we return the stored value; otherwise, we compute the result recursively and store it in the memo for future reference.
+
+*/
+
 class Solution {
 public:
     int climbStairs(int n, unordered_map<int, int>& memo) {
