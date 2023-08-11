addaa1ec3a2b33353687a0510e875337c9dda9cd
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-08-11 00:01:10 -0400

    added solution with memoization

---
+++ climbing_stairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        }
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
+    }
+};
