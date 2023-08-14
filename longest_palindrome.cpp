5b59c6a23e308a400d19556c543a88c67cfcc759
sabdulmajid <ayman.hasib@outlook.com>
2023-08-14 11:32:03 -0400

    code solution to the longest palindrome problem

--- longest_palindrome.cpp
+++ longest_palindrome.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int longestPalindrome(string s) {
-        
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+        for(char ch : s) {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+                oddCount++;
+            else    
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
     }
 };
