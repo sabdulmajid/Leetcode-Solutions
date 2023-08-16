9c304f2af1a63e56d274638bff1cd036599a8663
sabdulmajid <ayman.hasib@outlook.com>
2023-08-16 01:21:38 -0400

    added solution to problem

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -14,6 +14,13 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
+        ListNode *nextNode, *prevNode = NULL;
+        while (head) {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
     }
 };
