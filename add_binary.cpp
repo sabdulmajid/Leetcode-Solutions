51c7d8f4924c46d27eb7a4cdd0e615d1b8a1a12d
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-08-23 16:04:32 -0400

    added code explanation

--- add_binary.cpp
+++ add_binary.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /* Code Explanation
 
+This C++ code defines a class `Solution` that contains a method `addBinary` for adding two binary strings represented as input strings `a` and `b`. The method simulates binary addition by iterating through the input strings from their least significant digits to the most significant digits, while also considering a carry value. It adds corresponding digits from both strings along with the carry, updates the carry for the next iteration, and appends the sum of digits to the result string `ans`. The carry is then divided by 2 to prepare for the next addition. After processing both input strings, the result string is reversed to obtain the correct order of binary digits. Finally, the method returns the result as a binary string sum of the input strings.
 
 */
 
