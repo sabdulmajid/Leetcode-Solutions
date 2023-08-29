262c38193cc4e96ffd53cbbd58893b765ab4920f
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-08-29 00:02:59 -0400

    setting up code template for solution

---
+++ contains_duplicate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/* Code Explanation
+
+*/
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+    }
+};
