17bcdd62e6b0f09e50c535e230246042d5e825a8
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-01 02:25:56 -0400

    added code explanation to solution

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 /* Code Explanation
 
+This is a C++ code that checks if a given vector of integers `nums` contains any duplicates. The function `containsDuplicate` returns a boolean value of `true` if there are any duplicates, and `false` otherwise. The function uses an unordered map `seen` to keep track of the number of times each integer appears in the vector. It then iterates through the vector, incrementing the count for each integer in the map. If the count for an integer is greater than or equal to 1, it means that the integer has already appeared in the vector, so the function returns `true`. Otherwise, it continues iterating through the vector. This approach has a time complexity of O(n), where n is the length of the input vector.
+
 */
 
 class Solution {
