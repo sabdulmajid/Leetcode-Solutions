703823fcfd4e6f0867c540e5390587f6775eebab
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-02 02:28:02 -0400

    added code explanation for 'implementing a queue with a stack'

--- implement_queue_using_stack.cpp
+++ implement_queue_using_stack.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+/* Code Explanation
+
+This is a C++ code that defines a class MyQueue that implements a queue using two stacks. The class has four methods: push, pop, peek, and empty.
+
+1. The push method adds an element to the queue by pushing it onto the inp stack.
+
+2. The pop method removes and returns the first element in the queue by popping elements from the out stack. If the out stack is empty, it first transfers all elements from the inp stack to the out stack.
+
+3. The peek method returns the first element in the queue without removing it. If the out stack is empty, it first transfers all elements from the inp stack to the out stack.
+
+4. The empty method returns a boolean value indicating whether or not the queue is empty.
+
+This implementation of a queue using two stacks is known as a two-stack queue or a queue with amortized constant time complexity. The push and pop methods have an amortized time complexity of O(1), while the peek and empty methods have a time complexity of O(1).
+
+*/
+
 class MyQueue {
     stack<int> inp, out;
 public:
