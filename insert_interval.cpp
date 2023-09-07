d08e132d4cffc34edcb66e8c077e8c6cd98f2595
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-07 01:18:46 -0400

    set up code file for solution

---
+++ insert_interval.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* Code Explanation
+
+
+
+*/
+
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+    }
+};
