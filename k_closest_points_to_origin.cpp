fc5744eeb42298294a97814544364d3ea2e4b9d7
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-14 00:00:51 -0400

    set up code for solution

---
+++ k_closest_points_to_origin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* Code Explanation
+
+
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+    }
+};
