264504d1f711ad12a19bd9f5ca734348a8e56694
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-19 02:46:14 -0400

    added code explanation

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+/* Code Explanation
+
+The code defines a class named `Solution` with a public method `lengthOfLongestSubstring` that takes a string `s` as input and returns the length of the longest substring without repeating characters. The method uses a sliding window technique, represented by the variable `start`, to keep track of the starting index of the current substring without duplicates. A vector `dict` of size 256 is used to store the most recent index where each character appears in the string. As the loop iterates over the string, it updates `start` and `dict` accordingly and calculates the maximum length of such a substring encountered so far, stored in `maxLen`.
+
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
