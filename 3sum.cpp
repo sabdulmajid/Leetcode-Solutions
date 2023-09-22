b845f0553266872e022fcad3518874b18cfd7bc1
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-22 01:03:53 -0400

    added code explanation to solution

--- 3sum.cpp
+++ 3sum.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+/* Code Explanation
+
+The code defines a class named Solution with a public method threeSum. This method takes a vector of integers nums as input and returns all unique triplets in the array which sum up to zero. Firstly, the array is sorted. Then, by using a fixed element and two pointers (one starting right after the fixed element and the other at the end of the array), the method tries to find pairs whose sum with the fixed element equals zero. If the sum is less than zero, the left pointer (low) moves to the right; if it's greater, the right pointer (high) moves to the left. When a valid triplet is found, it's added to the output vector. To avoid duplicate results, both pointers skip over any subsequent duplicate values. Similarly, the outer loop skips over duplicates of the fixed element to further ensure uniqueness of the results.
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
