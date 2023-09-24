d9b3ebc8980292bd8c6db9d64f1eed4104af342c
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-09-24 05:50:14 -0400

    added code solution

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -14,6 +14,17 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode* head = new ListNode(0);
+        ListNode* cur = head;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int num = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = num / 10;
+            cur->next = new ListNode(num % 10);
+            cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        return head->next;
     }
 };
