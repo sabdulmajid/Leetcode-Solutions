e7c71603975a7e797c95e823e97198b84fa49abd
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-10-03 02:11:41 -0400

    added code explanation

--- evaluate_reverse_polish_notation.cpp
+++ evaluate_reverse_polish_notation.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 /* Code Explanation
 
+This code defines a class named "Solution" with a public member function evalRPN that evaluates Reverse Polish Notation (RPN) expressions provided as a vector of strings named "tokens". The function uses an unordered_map, which maps arithmetic operators (e.g., "+", "-", "*", "/") to corresponding lambda functions for performing arithmetic operations. A stack is employed to help with the evaluation: when a number is encountered in the tokens, it's pushed onto the stack; when an operator is encountered, two numbers are popped from the stack, the operation is performed, and the result is pushed back onto the stack. Once all tokens are processed, the result of the RPN expression is the top value on the stack.
+
 */
 
 class Solution {
