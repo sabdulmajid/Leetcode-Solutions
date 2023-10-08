6cf54ddfe25421ce251af514ec7545433b49b648
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-10-07 20:08:41 -0400

    initialized code solution file

---
+++ course_schedule.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* Code Explanation
+
+
+*/
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // TODO: Implement course scheduling algorithm
+        return false;
+    }
+};
