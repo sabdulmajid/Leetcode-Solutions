7fdd238515496aa9ce8d7d56b9feeaced8df1484
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-10-15 23:17:21 -0400

    added code explanation

--- coin_change.cpp
+++ coin_change.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 /* Code Explanation
 
+The Solution class contains a function named coinChange, which determines the minimum number of coins required to make up a given amount n using the coin denominations provided in the coins vector. The solution employs dynamic programming for efficiency. A dp array is used where dp[i] represents the minimum number of coins required to get the amount i. The array is initialized such that dp[0] is 0 (because zero coins are needed for amount 0) and all other values are set to INT_MAX (representing an unreachable amount). The coins are sorted for efficiency. For each amount i, the function iterates through the coins and checks if subtracting the current coin's value from i results in a valid index of dp. If it does, and the amount i-c was reachable (i.e., dp[i-c] is not INT_MAX), the function updates dp[i] with the minimum of its current value and 1 + dp[i-c]. After populating the dp array, if dp[n] is INT_MAX, the function returns -1 (indicating that amount n can't be made up using the given coin denominations); otherwise, it returns the value at dp[n], which is the minimum number of coins required.
+
 */
 
 class Solution {
