4c6c6321c5b80ba819534b85e5f559b422cd1f18
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-10-18 23:20:34 -0400

    added code explanation to solution

--- product_of_array_except_self.cpp
+++ product_of_array_except_self.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 /* Code Explanation
 
+The Solution class has a function productExceptSelf that calculates, for each position in the input nums vector, the product of all numbers in the vector except the number at that position. The function first populates the output vector with products of all numbers to the left of each index by iterating from left to right. Then, it adjusts these values with products of numbers to the right of each index by iterating from right to left using a right variable to accumulate the right-side products. The result is a vector where each entry gives the product of all numbers in the original vector except for the number at the corresponding position.
+
 */
 
 class Solution {
