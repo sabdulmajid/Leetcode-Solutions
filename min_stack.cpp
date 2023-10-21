a46b9cb4e59f981721c5b73901c3d5ed6da3d471
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-10-20 23:22:27 -0400

    added code solution

--- min_stack.cpp
+++ min_stack.cpp
@@ -9,25 +9,22 @@ using namespace std;
 
 class MinStack {
 public:
-    MinStack() {
-        
-    }
+    vector< pair<int,int> > s;
+	
+    MinStack() { }
     
     void push(int val) {
-        
+        if(s.empty())
+            s.push_back({val,val});
+        else
+            s.push_back({val,min(s.back().second,val)});    
     }
     
-    void pop() {
-        
-    }
+    void pop() { s.pop_back(); }
     
-    int top() {
-        
-    }
+    int top() { return s.back().first; }
     
-    int getMin() {
-        
-    }
+    int getMin() { return s.back().second; }
 };
 
 /**
